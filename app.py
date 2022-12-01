b3dd0e106e6b38c3c31d19b07ed0430f875b005c
Den-Laptop <zmaev.vsu@gmail.com>
2022-12-01 19:38:45 +0300

    Remove rudinant values

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 import sqlite3
 
-from app_config import app, db, mod
+from app_config import app, db
 from model import *
 from forms import *
-from flask import render_template, request, abort, flash, redirect, url_for
-from flask_paginate import Pagination, get_page_parameter
-
+from flask import render_template, request, flash, redirect, url_for
 
 
 def get_client(client_id):
+    """Get client by id from database"""
     return db.session.query(Client)\
         .filter(Client.client_id == int(client_id)).one_or_none()
 
 
 def get_vehicle(vin_number):
+    """Get vehicle by id from database"""
     return db.session.query(Vehicle)\
         .filter(Vehicle.vin_number == vin_number).one_or_none()
 
 
 def get_parking(parking_id):
+    """Get parking place by id from database"""
     return db.session.query(Parking)\
         .filter(Parking.parking_id == parking_id).one_or_none()
 
 
 def get_rent(rent_id):
+    """Get rent by id from database"""
     return db.session.query(Rent)\
         .filter(Rent.rent_id == rent_id).one_or_none()
 
 
-############
-# Clients #
-############
+# Clients
 
 
 @app.route('/client/<int:client_id>')
@@ -48,18 +48,11 @@ def client(client_id):
 def create_client():
     form = CreateClientForm()
     if request.method == 'POST' and form.validate_on_submit():
-        firstname = form.firstname.data
-        surname = form.surname.data
-        violation = form.violation.data
-
-        if not firstname or not surname or not violation:
-            flash('Please fill all fields')
-        else:
-            new_client = Client()
-            form.populate_obj(new_client)
-            db.session.add(new_client)
-            db.session.commit()
-            return redirect(url_for('clients'))
+        new_client = Client()
+        form.populate_obj(new_client)
+        db.session.add(new_client)
+        db.session.commit()
+        return redirect(url_for('clients'))
     return render_template(
         './client/create-client.html',
         form=form
@@ -76,9 +69,6 @@ def edit_client(client_id):
         if cur_client.violation == 1:
             form.violation.default = 1
     if request.method == 'POST' and form.validate_on_submit():
-        firstname = form.firstname.data
-        surname = form.surname.data
-        violation = form.violation.data
         form.populate_obj(cur_client)
         if form.delete.data:
             db.session.delete(cur_client)
@@ -86,13 +76,10 @@ def edit_client(client_id):
             flash('Client was successfully deleted!')
             return redirect(url_for('clients', client_id=client_id))
         
-        if not firstname or not surname or not violation:
-            flash('Please fill all fields')
-        else:
-            form.populate_obj(cur_client)
-            db.session.add(cur_client)       
-            db.session.commit()
-            return redirect(url_for('clients'))
+        form.populate_obj(cur_client)
+        db.session.add(cur_client)       
+        db.session.commit()
+        return redirect(url_for('clients'))
     return render_template('./client/edit-client.html', client=cur_client, form=form)
 
 
@@ -106,9 +93,7 @@ def clients():
         clients=clients)
 
 
-###########
-# Vehicle #
-###########
+# Vehicle
 
 
 @app.route('/vehicle/<string:vin_number>')
@@ -125,19 +110,11 @@ def vehicle(vin_number):
 def create_vehicle():
     form = CreateVehicleForm()
     if request.method == 'POST' and form.validate_on_submit():
-        vin_number = form.vin_number
-        brand = form.brand
-        price = form.price
-        condition = form.condition
-
-        if not vin_number or not brand or not price or not condition:
-            flash('Please fill all fields')
-        else:
-            new_vehicle = Vehicle()
-            form.populate_obj(new_vehicle)
-            db.session.add(new_vehicle)
-            db.session.commit()
-            return redirect(url_for('vehicles'))
+        new_vehicle = Vehicle()
+        form.populate_obj(new_vehicle)
+        db.session.add(new_vehicle)
+        db.session.commit()
+        return redirect(url_for('vehicles'))
     return render_template(
         './vehicle/create-vehicle.html',
         form=form
@@ -155,10 +132,6 @@ def edit_vehicle(vin_number):
         if cur_vehicle.condition == 1:
             form.condition.default = '1'
     if request.method == 'POST' and form.validate_on_submit():
-        vin_number = form.vin_number.data
-        brand = form.brand.data
-        price = form.price.data
-        condition = form.condition.data
         form.populate_obj(cur_vehicle)
         if form.delete.data:
             db.session.delete(cur_vehicle)
@@ -166,13 +139,10 @@ def edit_vehicle(vin_number):
             flash('Vehicle was successfully deleted!')
             return redirect(url_for('vehicles', vin_number=vin_number))
         
-        if not vin_number or not brand or not price:
-            flash('Please fill all fields')
-        else:
-            form.populate_obj(cur_vehicle)
-            db.session.add(cur_vehicle)       
-            db.session.commit()
-            return redirect(url_for('vehicles'))
+        form.populate_obj(cur_vehicle)
+        db.session.add(cur_vehicle)       
+        db.session.commit()
+        return redirect(url_for('vehicles'))
     return render_template(
         './vehicle/edit-vehicle.html',
         vehicle=cur_vehicle,
@@ -181,6 +151,8 @@ def edit_vehicle(vin_number):
 
 @app.route('/vehicle-list')
 def vehicles():
+    """ Rendering all vehicles from database """
+    # Pagination
     page = request.args.get('page', 1, type=int)
     vehicles = Vehicle.query.paginate(page=page, per_page=20)
     return render_template(
@@ -188,9 +160,7 @@ def vehicles():
         vehicles=vehicles)
 
 
-###########
-# Parking #
-###########1
+# Parking
 
 
 @app.route('/parking/<int:parking_id>')
@@ -207,16 +177,11 @@ def parking(parking_id):
 def create_parking():
     form = CreateParkingForm()
     if request.method == 'POST' and form.validate_on_submit():
-        vin_number = form.vin_number
-
-        if not vin_number:
-            flash('Please fill all fields')
-        else:
-            new_parking = Parking()
-            form.populate_obj(new_parking)
-            db.session.add(new_parking)
-            db.session.commit()
-            return redirect(url_for('parkings'))
+        new_parking = Parking()
+        form.populate_obj(new_parking)
+        db.session.add(new_parking)
+        db.session.commit()
+        return redirect(url_for('parkings'))
     return render_template(
         './parking/create-parking.html',
         form=form
@@ -239,13 +204,10 @@ def edit_parking(parking_id):
             return redirect(url_for('parkings',
              vin_number=vin_number))
         
-        if not vin_number:
-            flash('Please fill all fields')
-        else:
-            form.populate_obj(cur_parking)
-            db.session.add(cur_parking)       
-            db.session.commit()
-            return redirect(url_for('parkings'))
+        form.populate_obj(cur_parking)
+        db.session.add(cur_parking)       
+        db.session.commit()
+        return redirect(url_for('parkings'))
     return render_template(
         './parking/edit-parking.html',
         parking=cur_parking,
@@ -254,6 +216,8 @@ def edit_parking(parking_id):
 
 @app.route('/parkings')
 def parkings():
+    """ Rendering parking table from database """
+    # Pagination
     page = request.args.get('page', 1, type=int)
     parkings = Parking.query.paginate(page=page, per_page=20)
     return render_template(
@@ -261,9 +225,7 @@ def parkings():
         parkings=parkings)
 
 
-########
-# Rent #
-########
+# Rent
 
 
 @app.route('/rent/<int:rent_id>')
@@ -280,20 +242,11 @@ def rent(rent_id):
 def create_rent():
     form = CreateRentForm()
     if request.method == 'POST' and form.validate_on_submit():
-        client_id = form.client_id
-        vin_number = form.vin_number
-        begin_date = form.begin_date
-        end_date = form.end_date
-
-        if not vin_number or not client_id\
-             or not begin_date or not end_date:
-            flash('Please fill all fields')
-        else:
-            new_rent = Rent()
-            form.populate_obj(new_rent)
-            db.session.add(new_rent)
-            db.session.commit()
-            return redirect(url_for('rents'))
+        new_rent = Rent()
+        form.populate_obj(new_rent)
+        db.session.add(new_rent)
+        db.session.commit()
+        return redirect(url_for('rents'))
     return render_template(
         './rent/create-rent.html',
         form=form
@@ -310,10 +263,6 @@ def edit_rent(rent_id):
         form.begin_date.data = cur_rent.begin_date
         form.end_date.data = cur_rent.end_date
     if request.method == 'POST' and form.validate_on_submit():
-        client_id = form.client_id.data
-        vin_number = form.vin_number.data
-        begin_date = form.begin_date.data
-        end_date = form.end_date.data
         form.populate_obj(cur_rent)
         if form.delete.data:
             db.session.delete(cur_rent)
@@ -322,14 +271,10 @@ def edit_rent(rent_id):
             return redirect(url_for('rents',
              rent_id=rent_id))
         
-        if not vin_number or not client_id or not begin_date\
-            or not end_date:
-            flash('Please fill all fields')
-        else:
-            form.populate_obj(cur_rent)
-            db.session.add(cur_rent)       
-            db.session.commit()
-            return redirect(url_for('rents'))
+        form.populate_obj(cur_rent)
+        db.session.add(cur_rent)       
+        db.session.commit()
+        return redirect(url_for('rents'))
     return render_template(
         './rent/edit-rent.html',
         rent=cur_rent,
